dfff7fa2f390a8f10fb5d970dcfe424cf244bbf6
MatSobol <mateuszsobol46@gmail.com>
2023-11-30 23:52:17 +0100

    implemented_generetion_of_spline

--- Assets/RoadGenerator.cs
+++ Assets/RoadGenerator.cs
@@ -6,29 +6,36 @@ using System.Security.Cryptography;
 using UnityEngine;
 
 
+public enum RoadType
+{
+    Empty, Obstacle, Road
+}
+
 public class RoadGenerator : MonoBehaviour
 {
 
-    [SerializeField] GameObject road_straight;
-    [SerializeField] GameObject road_turn;
-    [SerializeField] GameObject road_3way;
-    [SerializeField] GameObject road_4way;
     [SerializeField] ProceduralTerrain theTerrain;
     // AABB of the plane
-    Vector2 plane_min_corner, plane_max_corner;
-    int num_cells_in_row;
+    public Vector2 plane_min_corner, plane_max_corner;
+    public int num_cells_in_row;
     [SerializeField] int road_tile_size = 2;
     // % chance to generate junction in [0, 100]
     [SerializeField] int chance_to_pick_dir;
     [SerializeField] int number_of_iterations;
+    [SerializeField] int max_num_4way_intersections = 60;
 
-    enum RoadType
-    {
-        Empty, Obstacle, Road
-    }
     // Square grid that holds information about the road system
-    RoadType[,] road_type_grid;
+    public RoadType[,] road_type_grid;
 
+    public int GetRoadTileSize()
+    {
+        return road_tile_size;
+    }
+
+    public ProceduralTerrain GetTerrain()
+    {
+        return theTerrain;
+    }
 
     void Start()
     {
@@ -42,7 +49,9 @@ public class RoadGenerator : MonoBehaviour
         road_type_grid = new RoadType[num_cells_in_row, num_cells_in_row];
         InitializeRoadTypeGrid();
         GenerateRoad(new Vector2Int(num_cells_in_row / 2, num_cells_in_row / 2), number_of_iterations); // start from center and use 5 iterations
-        DrawRoadBasedOnRoadTypeGrid(); // using data in road_type_grid, instantiate correct prefabs to visualize the road.
+        RoadTools rt = new RoadTools(this);
+        rt.ExtrudeDeadEndingRoads();
+        rt.DeleteRandom4WayIntersectionsUntilUnderLimit(max_num_4way_intersections);
     }
 
     /*
@@ -95,12 +104,12 @@ public class RoadGenerator : MonoBehaviour
         {
             neighbours_in_current_and_previous = checkIfAnyNeighboursNotIndirection(array_dimension, current_pos, neighbours_in_current_and_previous, out previousIsAlreadyExistingRoad);
             // Counts to 2 because some roades ends one tile before border (suprisingly often)  
-            if (neighbours_in_current_and_previous == 2) 
+            if (neighbours_in_current_and_previous == 2)
             {
                 if (previousIsAlreadyExistingRoad) { break; }
                 current_pos -= direction;
                 road_type_grid[current_pos.x, current_pos.y] = RoadType.Empty;
-                break; 
+                break;
             }
             road_type_grid[current_pos.x, current_pos.y] = RoadType.Road;
             current_pos += direction;
@@ -111,7 +120,7 @@ public class RoadGenerator : MonoBehaviour
     int checkIfAnyNeighboursNotIndirection(int array_dimension, Vector2Int grid_pos, int neighbours_in_current_and_previous, out bool previousIsAlreadyExistingRoad)
     {
         //if encounter existing road change previousIsAlreadyExistingRoad to true so it is not deleted
-        if (road_type_grid[grid_pos.x, grid_pos.y] == RoadType.Road) { previousIsAlreadyExistingRoad = true;  return 1; }
+        if (road_type_grid[grid_pos.x, grid_pos.y] == RoadType.Road) { previousIsAlreadyExistingRoad = true; return 1; }
         else { previousIsAlreadyExistingRoad = false; }
         switch (array_dimension)
         {
@@ -119,7 +128,7 @@ public class RoadGenerator : MonoBehaviour
                 bool up = false, down = false;
                 if (grid_pos.y > 0) { down = road_type_grid[grid_pos.x, grid_pos.y - 1] == RoadType.Road; }
                 if (grid_pos.y < num_cells_in_row - 1) { up = road_type_grid[grid_pos.x, grid_pos.y + 1] == RoadType.Road; }
-                if(up || down)
+                if (up || down)
                 {
                     return ++neighbours_in_current_and_previous;
                 }
@@ -135,166 +144,6 @@ public class RoadGenerator : MonoBehaviour
                 return 0;
         }
         throw new Exception("Bad value of array_dimension");
-    } 
-
-    void DrawRoadBasedOnRoadTypeGrid()
-    {
-        for (int i = 0; i < num_cells_in_row; ++i)
-        {
-            for (int j = 0; j < num_cells_in_row; ++j)
-            {
-                switch (road_type_grid[i, j])
-                {
-                    case RoadType.Road:
-                        Quaternion rotation;
-                        var road = Instantiate(GetRoadVariantBasedOnNeighboringTiles(new Vector2Int(i, j), out rotation));
-                        DrawRoadBasedOnAngle(road, rotation, i, j);
-                        break;
-                    default:
-                        break;
-                }
-            }
-        }
-    }
-    /*
-     This method will create a road with propare angle based on difference between the heights  
-     */
-    void DrawRoadBasedOnAngle (GameObject road, Quaternion rotation, int i, int j)
-    {
-
-        float pos_x = plane_min_corner.x + road_tile_size * i, pos_z = plane_min_corner.y + road_tile_size * j;
-
-        float [] start_pos = new float[2];
-        float [] end_pos = new float[2];
-
-        //checking if the road is turned and based on it change positions
-        switch (rotation.eulerAngles.y)
-        {
-            case 0.0f:
-                start_pos[0] = pos_x + road_tile_size / 2;
-                start_pos[1] = pos_z;
-                end_pos[0] = pos_x + road_tile_size / 2;
-                end_pos[1] = pos_z + road_tile_size;
-                break;
-            case 90.0f:
-                start_pos[0] = pos_x;
-                start_pos[1] = pos_z + road_tile_size / 2;
-                end_pos[0] = pos_x + road_tile_size;
-                end_pos[1] = pos_z + road_tile_size / 2;
-                break;
-            case 180.0f:
-                start_pos[0] = pos_x + road_tile_size / 2;
-                start_pos[1] = pos_z + road_tile_size;
-                end_pos[0] = pos_x + road_tile_size / 2;
-                end_pos[1] = pos_z;
-                break;
-            case 270.0f:
-                start_pos[0] = pos_x + road_tile_size;
-                start_pos[1] = pos_z + road_tile_size / 2;
-                end_pos[0] = pos_x;
-                end_pos[1] = pos_z + road_tile_size / 2;
-                break;
-            default:
-                throw new Exception("Incorrect angle of turn");
-        }
-
-        // for the time being since there is no map i will manually do the heights but normally it will be based on start_pos and end_pos
-        //height where road starts
-        Vector3 signPosition = new Vector3(start_pos[0], 0, start_pos[1]);
-        float start_height = theTerrain.terrain.SampleHeight(signPosition);
-        //height where end of the road
-        Vector3 signPosition2 = new Vector3(end_pos[0], 0, end_pos[1]);
-        float end_height = theTerrain.terrain.SampleHeight(signPosition2);
-
-        float difference = start_height - end_height;
-
-        if(difference == 0.0f) 
-        {
-             // to be implemented get height of previous tile and give it rather than 0.1f
-            road.GetComponent<Transform>().position = new Vector3(pos_x + road_tile_size / 2, start_height, pos_z + road_tile_size / 2);
-            road.GetComponent<Transform>().rotation = rotation * (road.GetComponent<Transform>().rotation);
-        }
-        else if(difference < 0.0f) 
-        {
-            float angleInRadians = Mathf.Atan2((-difference)/2, road_tile_size / 2);
-            float angleInDegrees = Mathf.Rad2Deg * angleInRadians;
-            road.GetComponent<Transform>().position = new Vector3(pos_x + road_tile_size / 2, start_height + (-difference) / 2, pos_z + road_tile_size / 2);
-            road.GetComponent<Transform>().rotation = rotation * (road.GetComponent<Transform>().rotation * Quaternion.Euler((-angleInDegrees), 0.0f, 0.0f));
-            float scaleY = GetProperScale(difference);
-            road.GetComponent<Transform>().localScale += new Vector3(0,scaleY,0);
-        }
-        else
-        {
-            float angleInRadians = Mathf.Atan2(difference / 2, road_tile_size / 2);
-            float angleInDegrees = Mathf.Rad2Deg * angleInRadians;
-            road.GetComponent<Transform>().position = new Vector3(pos_x + road_tile_size / 2, start_height - difference / 2, pos_z + road_tile_size / 2);
-            road.GetComponent<Transform>().rotation = rotation * (road.GetComponent<Transform>().rotation * Quaternion.Euler(angleInDegrees, 0.0f, 0.0f));
-            float scaleY = GetProperScale(difference);
-            road.GetComponent<Transform>().localScale += new Vector3(0, scaleY, 0);
-        }
-    }
-    // need to scale since diagonal is longer
-    float GetProperScale(float difference)
-    {
-        float supposed_length = Mathf.Sqrt(difference * difference + road_tile_size * road_tile_size);
-        float result = supposed_length / road_tile_size * 100 - 100;
-        return result;
-    }
-
-
-    /*
-        Picks matching road tile according to it's neighbors
-        and also calculates neccessary orientation to fit them.
-    */
-    GameObject GetRoadVariantBasedOnNeighboringTiles(Vector2Int grid_pos, out Quaternion orientation)
-    {
-        bool up = false, down = false, left = false, right = false;
-        if (grid_pos.y > 0) { down = road_type_grid[grid_pos.x, grid_pos.y - 1] == RoadType.Road; }
-        if (grid_pos.y < num_cells_in_row - 1) { up = road_type_grid[grid_pos.x, grid_pos.y + 1] == RoadType.Road; }
-        if (grid_pos.x > 0) { left = road_type_grid[grid_pos.x - 1, grid_pos.y] == RoadType.Road; }
-        if (grid_pos.x < num_cells_in_row - 1) { right = road_type_grid[grid_pos.x + 1, grid_pos.y] == RoadType.Road; }
-
-        int num_of_non_empty_neighbors = 0;
-        if (up) { ++num_of_non_empty_neighbors; }
-        if (down) { ++num_of_non_empty_neighbors; }
-        if (left) { ++num_of_non_empty_neighbors; }
-        if (right) { ++num_of_non_empty_neighbors; }
-
-        orientation = Quaternion.identity;
-        switch (num_of_non_empty_neighbors)
-        {
-            case 1:
-                if (up || down) { orientation = Quaternion.identity; }
-                else { orientation = Quaternion.AngleAxis(90.0f, Vector3.up); }
-                return road_straight;
-            case 2:
-                if (up && down)
-                {
-                    orientation = Quaternion.identity;
-                    return road_straight;
-                }
-                if (left && right)
-                {
-                    orientation = Quaternion.AngleAxis(90.0f, Vector3.up);
-                    return road_straight;
-                }
-
-                if (up && left) { orientation = Quaternion.identity; }
-                else if (up && right) { orientation = Quaternion.AngleAxis(90.0f, Vector3.up); }
-                else if (right && down) { orientation = Quaternion.AngleAxis(180.0f, Vector3.up); }
-                else if (down && left) { orientation = Quaternion.AngleAxis(270.0f, Vector3.up); }
-                return road_turn;
-            case 3:
-                if (right && left && down) { orientation = Quaternion.AngleAxis(90.0f, Vector3.up); }
-                else if (right && left && up) { orientation = Quaternion.AngleAxis(270.0f, Vector3.up); }
-                else if (up && down && right) { orientation = Quaternion.identity; }
-                else if (up && down && left) { orientation = Quaternion.AngleAxis(180.0f, Vector3.up); }
-                return road_3way;
-            case 4:
-                return road_4way;
-            default:
-                throw new Exception("Single road tile without any road neighbors is an error in the generating algorithm");
-        }
     }
 
     void InitializeRoadTypeGrid(RoadType init_type = RoadType.Empty)
--- Assets/spline.cs
+++ Assets/spline.cs
@@ -8,10 +8,14 @@ using Unity.Mathematics;
 
 public class spline : MonoBehaviour
 {
-    int number = 0;
-    List<float3> mpoint = new List<float3>(100);
-    List<float3> m_Reduced = new List<float3>(20);
     Spline[] m_Splines = { new Spline() };
+    int counter;
+    int number;
+    public RoadGenerator rg;
+    public SplineContainer splineContainer;
+    int num_cells_in_row;
+    List<List<float3>> mpoint;
+    bool[][] visited; 
 
     // Point reduction epsilon determines how aggressive the point reduction algorithm is when removing redundant
     // points. Lower values result in more accurate spline representations of the original line, at the cost of
@@ -21,46 +25,317 @@ public class spline : MonoBehaviour
 
     // Tension affects how "curvy" splines are at knots. 0 is a sharp corner, 1 is maximum curvitude.
     float m_SplineTension = 1f;
-
-
+    void FindFirstPoint(out int x, out int y)
+    {
+        for (int i = 0; i < num_cells_in_row; ++i)
+        {
+            for (int j = 0; j < num_cells_in_row; ++j)
+            {
+                if(rg.road_type_grid[i, j] == RoadType.Road)
+                {
+                    if(CheckNumberOfNeighbours(i,j) > 2)
+                    {
+                        x = i;
+                        y = j;
+                        return;
+                    }
+                }
+            }
+        }
+        throw new System.Exception("no 3-way or 4-way intersection");
+    }
 
     void Start()
     {
-        for (float i = 0; i < 100; i++)
+        CreatePoints();
+    }
+    // main function with create points
+    public void CreatePoints()
+    {
+        num_cells_in_row = rg.num_cells_in_row;
+        mpoint = new List<List<float3>>();
+        visited = new bool[num_cells_in_row][];
+        for (int i = 0;i < num_cells_in_row; i++) 
         {
-            mpoint.Add(new float3(i, 10.0f+i, 0.0f));
+            visited[i] = new bool[num_cells_in_row];
+            for (int j = 0; j < num_cells_in_row; j++)
+            {
+                visited[i][j] = false;
+            }
         }
-        RebuildSpline();
+
+        int x, y;
+        FindFirstPoint(out x, out y);
+        GoInDirections(x,y);
+
+        CreateSplines(mpoint.Count);
+        BuildSplineBasedOnPoints();
+    }
+    //Go into all directions from intersection or turn
+    void GoInDirections(int x, int y)
+    {
+        var road_type_grid = rg.road_type_grid;
+        var road_tile_size = rg.GetRoadTileSize();
+        var theTerrain = rg.GetTerrain();
+        //use only in loop
+        int gridX = x - 1;
+        int gridY = y;
+        //left
+        List<float3> listLeft = new List<float3>();
+        if(gridX >= 0)
+        {
+            if (!visited[gridX][gridY])
+            {
+                while (road_type_grid[gridX, gridY] == RoadType.Road)
+                {
+                    visited[gridX][gridY] = true;
+                    float realX = rg.plane_min_corner.x + road_tile_size * gridX, realY = rg.plane_min_corner.y + road_tile_size * gridY;
+                    if (CheckNeighboursNotInDirection(gridX, gridY, Direction.Left) == 0)
+                    {
+                        float[] start_pos = new float[2];
+                        start_pos[0] = realX + road_tile_size;
+                        start_pos[1] = realY + road_tile_size / 2;
+                        Vector3 signPosition = new Vector3(start_pos[0], 0, start_pos[1]);
+                        float start_height = theTerrain.terrain.SampleHeight(signPosition);
+                        listLeft.Add(new float3(start_pos[0], start_height, start_pos[1]));
+                    }
+                    else
+                    {
+                        float[] start_pos = new float[2];
+                        start_pos[0] = realX + road_tile_size;
+                        start_pos[1] = realY + road_tile_size / 2;
+                        Vector3 signPosition = new Vector3(start_pos[0], 0, start_pos[1]);
+                        float start_height = theTerrain.terrain.SampleHeight(signPosition);
+                        listLeft.Add(new float3(start_pos[0], start_height, start_pos[1]));
+                        GoInDirections(gridX, gridY);
+                        break;
+                    }
+                    gridX = gridX - 1;
+                }
+            }
+        }
+        if (listLeft.Count > 0)
+        {
+            mpoint.Add(listLeft);
+        }
+        gridX = x + 1;
+        gridY = y;
+        //right
+        List<float3> listRight = new List<float3>();
+        if(gridX < num_cells_in_row)
+        if (!visited[gridX][gridY])
+        {
+            while (road_type_grid[gridX, gridY] == RoadType.Road)
+            {
+                visited[gridX][gridY] = true;
+                float realX = rg.plane_min_corner.x + road_tile_size * gridX, realY = rg.plane_min_corner.y + road_tile_size * gridY;
+                if (CheckNeighboursNotInDirection(gridX, gridY, Direction.Right) == 0)
+                {
+                    float[] start_pos = new float[2];
+                    start_pos[0] = realX;
+                    start_pos[1] = realY + road_tile_size / 2;
+                    Vector3 signPosition = new Vector3(start_pos[0], 0, start_pos[1]);
+                    float start_height = theTerrain.terrain.SampleHeight(signPosition);
+                    listRight.Add(new float3(start_pos[0], start_height, start_pos[1]));
+                }
+                else
+                {
+                    float[] start_pos = new float[2];
+                    start_pos[0] = realX;
+                    start_pos[1] = realY + road_tile_size / 2;
+                    Vector3 signPosition = new Vector3(start_pos[0], 0, start_pos[1]);
+                    float start_height = theTerrain.terrain.SampleHeight(signPosition);
+                    listRight.Add(new float3(start_pos[0], start_height, start_pos[1]));
+                    GoInDirections(gridX, gridY);
+                    break;
+                }
+                gridX = gridX + 1;
+            }
+        }
+        if (listRight.Count > 0)
+        {
+            mpoint.Add(listRight);
+        }
+        gridX = x;
+        gridY = y + 1;
+        //top
+        List<float3> listTop = new List<float3>();
+        if (gridY < num_cells_in_row)
+        {
+            if (!visited[gridX][gridY])
+            {
+                while (road_type_grid[gridX, gridY] == RoadType.Road)
+                {
+                    visited[gridX][gridY] = true;
+                    float realX = rg.plane_min_corner.x + road_tile_size * gridX, realY = rg.plane_min_corner.y + road_tile_size * gridY;
+                    if (CheckNeighboursNotInDirection(gridX, gridY, Direction.Top) == 0)
+                    {
+                        float[] start_pos = new float[2];
+                        start_pos[0] = realX + road_tile_size / 2;
+                        start_pos[1] = realY;
+                        Vector3 signPosition = new Vector3(start_pos[0], 0, start_pos[1]);
+                        float start_height = theTerrain.terrain.SampleHeight(signPosition);
+                        listTop.Add(new float3(start_pos[0], start_height, start_pos[1]));
+                    }
+                    else
+                    {
+                        float[] start_pos = new float[2];
+                        start_pos[0] = realX + road_tile_size / 2;
+                        start_pos[1] = realY;
+                        Vector3 signPosition = new Vector3(start_pos[0], 0, start_pos[1]);
+                        float start_height = theTerrain.terrain.SampleHeight(signPosition);
+                        listTop.Add(new float3(start_pos[0], start_height, start_pos[1]));
+                        GoInDirections(gridX, gridY);
+                        break;
+                    }
+                    gridY = gridY + 1;
+                }
+            }
+        } 
+        if (listTop.Count > 0)
+        {
+            mpoint.Add(listTop);
+        }
+        //down
+        gridX = x;
+        gridY = y - 1;
+        List<float3> listDown = new List<float3>();
+        if(gridY >= 0)
+        {
+            if (!visited[gridX][gridY])
+            {
+                while (road_type_grid[gridX, gridY] == RoadType.Road)
+                {
+                    visited[gridX][gridY] = true;
+                    float realX = rg.plane_min_corner.x + road_tile_size * gridX, realY = rg.plane_min_corner.y + road_tile_size * gridY;
+                    if (CheckNeighboursNotInDirection(gridX, gridY, Direction.Bottom) == 0)
+                    {
+                        float[] start_pos = new float[2];
+                        start_pos[0] = realX + road_tile_size / 2;
+                        start_pos[1] = realY + road_tile_size;
+                        Vector3 signPosition = new Vector3(start_pos[0], 0, start_pos[1]);
+                        float start_height = theTerrain.terrain.SampleHeight(signPosition);
+                        listDown.Add(new float3(start_pos[0], start_height, start_pos[1]));
+                    }
+                    else
+                    {
+                        float[] start_pos = new float[2];
+                        start_pos[0] = realX + road_tile_size / 2;
+                        start_pos[1] = realY + road_tile_size;
+                        Vector3 signPosition = new Vector3(start_pos[0], 0, start_pos[1]);
+                        float start_height = theTerrain.terrain.SampleHeight(signPosition);
+                        listDown.Add(new float3(start_pos[0], start_height, start_pos[1]));
+                        GoInDirections(gridX, gridY);
+                        break;
+                    }
+                    gridY = gridY - 1;
+                }
+            }
+        }
+        if (listDown.Count > 0)
+        {
+            mpoint.Add(listDown);
+        }
     }
 
-    void RebuildSpline()
+    private enum Direction
     {
-        // Before setting spline knots, reduce the number of sample points.
-        SplineUtility.ReducePoints(mpoint, m_Reduced, m_PointReductionEpsilon);
+        Left, Right, Top, Bottom
+    }
 
-        var xd = GetComponent<SplineContainer>();
-        // give how many splines you need
-        number = 5;
+    int CheckNeighboursNotInDirection(int x, int y, Direction direction)
+    {
+        int counter = 0;
+        var road_type_grid = rg.road_type_grid;
+        if (direction == Direction.Left || direction == Direction.Right)
+        {
+            if (y > 0)
+            {
+                if (road_type_grid[x, y - 1] == RoadType.Road) counter++;
+            }
+            if (y < num_cells_in_row - 1)
+            {
+                if (road_type_grid[x, y + 1] == RoadType.Road) counter++;
+            }
+        }
+        if(direction == Direction.Top || direction == Direction.Bottom)
+        {
+            if (x > 0)
+            {
+                if (road_type_grid[x - 1, y] == RoadType.Road) counter++;
+            }
+            if (x < num_cells_in_row - 1)
+            {
+                if (road_type_grid[x + 1, y] == RoadType.Road) counter++;
+            }
+        }
+
+
+
+        return counter;
+    }
+
+    int CheckNumberOfNeighbours(int x, int y)
+    {
+        int counter = 0;
+        var road_type_grid = rg.road_type_grid;
+        if (y > 0)
+        {
+            if (road_type_grid[x, y - 1] == RoadType.Road) counter++;
+        }
+        if (y < num_cells_in_row - 1)
+        { 
+            if(road_type_grid[x, y + 1] == RoadType.Road) counter++;
+        }
+        if (x > 0)
+        { 
+            if(road_type_grid[x - 1, y] == RoadType.Road) counter++;
+        }
+        if (x < num_cells_in_row - 1)
+        { 
+            if(road_type_grid[x + 1, y] == RoadType.Road) counter++;
+        }
+        return counter;
+    }
+
+
+    void CreateSplines(int number)
+    {
+        // how many splines needed
+        this.number = number;
+        // Here initialize all necessary Splines in Holder
         // necessary to start set
         Holder = null;
         // here i give to SplineContainer Holder which must containts all Splines
-        xd.Splines = Holder;
-        // get Spline with index 1 
-        var spline = xd[1];
+        splineContainer.Splines = Holder;
+    }
 
-        // Assign the reduced sample positions to the Spline knots collection. Here we are constructing new
-        // BezierKnots from a single position, disregarding tangent and rotation. The tangent and rotation will be
-        // calculated automatically in the next step wherein the tangent mode is set to "Auto Smooth."
-        spline.Knots = m_Reduced.Select(x => new BezierKnot(x));
+    void BuildSplineBasedOnPoints()
+    {
+        int counter = 0;
+        foreach (List<float3> list in mpoint)
+        {
+            //List<float3> m_Reduced = new List<float3>();
+            // Before setting spline knots, reduce the number of sample points.
+            //SplineUtility.ReducePoints(list, m_Reduced, m_PointReductionEpsilon);
 
-        var all = new SplineRange(0, spline.Count);
+            var spline = splineContainer[counter];
 
-        // Sets the tangent mode for all knots in the spline to "Auto Smooth."
-        spline.SetTangentMode(all, TangentMode.AutoSmooth);
+            // Assign the reduced sample positions to the Spline knots collection. Here we are constructing new
+            // BezierKnots from a single position, disregarding tangent and rotation. The tangent and rotation will be
+            // calculated automatically in the next step wherein the tangent mode is set to "Auto Smooth."
+            spline.Knots = list.Select(x => new BezierKnot(x));
 
-        // Sets the tension parameter for all knots. Note that the "Tension" parameter is only applicable to
-        // "Auto Smooth" mode knots.
-        spline.SetAutoSmoothTension(all, m_SplineTension);
+            var all = new SplineRange(0, spline.Count);
+
+            // Sets the tangent mode for all knots in the spline to "Auto Smooth."
+            spline.SetTangentMode(all, TangentMode.AutoSmooth);
+
+            // Sets the tension parameter for all knots. Note that the "Tension" parameter is only applicable to
+            // "Auto Smooth" mode knots.
+            spline.SetAutoSmoothTension(all, m_SplineTension);
+            counter ++;
+        }
     }
 
     // this type of value (IReadOnlyList) is needed to create new spline
