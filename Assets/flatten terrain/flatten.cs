da78f05c6cf6db38897888e86c12e15b57e320fd
MatSobol <mateuszsobol46@gmail.com>
2023-12-27 18:50:15 +0100

    refactorization

--- Assets/flatten terrain/flatten.cs
+++ Assets/flatten terrain/flatten.cs
@@ -5,165 +5,273 @@ using System;
 using UnityEngine.UIElements;
 using Unity.VisualScripting;
 using System.Collections.Generic;
+using TreeEditor;
 
 public class Flatten : MonoBehaviour
 {
-    public int startX = 1;
-    public int endX = 2;
-    public int startZ = 10;
-    public int endZ = 12;
-    public float targetHeight = 20f;
-    public RoadGenerator rg;
-    public ProceduralTerrain proceduralTerain;
-    private void Start()
-    {
+    public ProceduralTerrain terrain;
+    float terrainTileSize;
 
-    }
-    public void flattenBasedOnPoint(List<float3> positions, float streetWidth)
+    //"Optional shaped ramp around perimeter."
+    public AnimationCurve PerimeterRampCurve = AnimationCurve.Linear(0, 0, 1, 1);
+
+    //Size of gaussian filter applied to change array. Set to zero for none"
+    public int PerimeterRampDistance = 1;
+
+    //"Use Perimeter Ramp Curve in lieu of direct gaussian smooth."
+    public bool ApplyPerimeterRampCurve = false;
+
+    // This extends the binary on/off blend stencil out by one pixel,
+    // making one sheet at a time, then stacks (adds) them all together and
+    // renormalizes them back to 0.0-1.0.
+    //
+    // it simultaneously takes the average of the "hitting" perimeter neighboring
+    // heightmap cells and extends it outwards as it expands.
+    //
+    void GeneratePerimeterHeightRampAndFlange(float[,] heightMap, float[,] blendStencil, int distance)
     {
-        proceduralTerain = rg.GetTerrain();
-        List<float3> data = new List<float3>(positions);
-        data.Sort((a, b) => -a.y.CompareTo(b.y));
-        foreach (float3 position in data)
+        int w = blendStencil.GetLength(0);
+        int h = blendStencil.GetLength(1);
+
+        // each stencil, expanded by one more pixel, before we restack them
+        float[][,] stencilPile = new float[distance + 1][,];
+
+        // where we will build the horizontal heightmap flange out
+        float[,] extendedHeightmap = new float[w, h];
+
+        // directonal table: 4-way and 8-way available
+        int[] neighborXYPairs = new int[] {
+			// compass directions first
+			0, 1,
+            1, 0,
+            0, -1,
+            -1, 0,
+			// diagonals next
+			1,1,
+            -1,1,
+            1,-1,
+            -1,-1,
+        };
+
+        int neighborCount = 4;                  // 4 and 8 are supported from the table above
+
+        float[,] source = blendStencil;         // this is NOT a copy! This is a reference!
+        for (int n = 0; n <= distance; n++)
         {
-            int width = Mathf.FloorToInt(streetWidth);
-            //check if position is on side or corner of square
-            bool left = position.x - Math.Floor(position.x) == 0;
-            bool right = position.x - Math.Ceiling(position.x) == 0;
-            bool down = position.z - Math.Floor(position.z) == 0;
-            bool top = position.z - Math.Ceiling(position.z) == 0;
+            // add it to the pile BEFORE we expand it;
+            // that way the first one is the original
+            // input blendStencil.
+            stencilPile[n] = source;
 
-            if (left || right || down || top)
+            // Debug: WritePNG( source, "pile-" + n.ToString());
+
+            // this is gonna be an actual true deep copy of the stencil
+            // as it stands now, and it will steadily grow outwards, but
+            // each time it is always 0.0 or 1.0 cells, nothing in between.
+            float[,] expanded = new float[w, h];
+            for (int j = 0; j < h; j++)
             {
-                isInsideEdge(position, width, left, right, top, down);
+                for (int i = 0; i < w; i++)
+                {
+                    expanded[i, j] = source[i, j];
+                }
             }
-            else
+
+            // we have to quit so we don't further expand the flange heightmap
+            if (n == distance)
             {
-                isInsideSquare(position, width);
+                break;
             }
 
-            //very simplified solution for not straight for now
-            //isInsideSquare(position, width);
-        }
-    }
-
-    public void isInsideSquare(Vector3 position, int width)
-    {
-        int leftBottomX = Mathf.FloorToInt(position.x);
-        int leftBottomZ = Mathf.FloorToInt(position.z);
-
-        // Mathf.Floor -> round down Mathf.Ceil -> round up           
-        int4 squareInWhichIsPoint = new int4(leftBottomX, leftBottomZ, leftBottomX + 1, leftBottomZ +1);
-        SetTerrainHeightForArea(squareInWhichIsPoint, position.y);
-    }
-    public void isInsideEdge(Vector3 position, int width, bool left, bool right, bool top, bool down)
-    {
-        int leftBottomX = Mathf.FloorToInt(position.x);
-        int leftBottomZ = Mathf.FloorToInt(position.z);
-        int4 squareToFlatten = new int4();
-        // check if inside corner
-        if ( (left && down && top && right))
-        {
-            squareToFlatten = new int4((int)position.x-1, (int)position.z-1, (int)position.x + 1, (int)position.z + 1);
-        }
-
-        else if (left)
-        {
-            squareToFlatten = new int4((int)position.x - 1, (int)position.z, (int)position.x + 1, (int)position.z + 1);
-        }
-
-        else if (right)
-        {
-            squareToFlatten = new int4((int)position.x, (int)position.z, (int)position.x + 2, (int)position.z + 1);
-        }
-
-        else if (top)
-        {
-            squareToFlatten = new int4((int)position.x, (int)position.z, (int)position.x + 1, (int)position.z + 2);
-        }
-
-        else if (down)
-        {
-            squareToFlatten = new int4((int)position.x, (int)position.z+1, (int)position.x + 1, (int)position.z - 1);
-        }
-        Vector3 signPosition = new Vector3(position.x, 0, position.z);
-        //simpleSetTerrainHeightForSquare(squareToFlatten, proceduralTerain.terrain.SampleHeight(signPosition)/proceduralTerain.depth);
-        simpleSetTerrainHeightForSquare(squareToFlatten, proceduralTerain.GetHeight(leftBottomZ, leftBottomX));
-    }
-    //simplified version just for now
-    public void simpleSetTerrainHeightForSquare(int4 area, float targetHeight)
-    {
-        TerrainData terrainData = proceduralTerain.terrain.terrainData;
-        float[,] heights = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
-        startX = Mathf.Clamp(area[0], 0, terrainData.heightmapResolution - 1);
-        endX = Mathf.Clamp(area[2], 0, terrainData.heightmapResolution - 1);
-        startZ = Mathf.Clamp(area[1], 0, terrainData.heightmapResolution - 1);
-        endZ = Mathf.Clamp(area[3], 0, terrainData.heightmapResolution - 1);
-        for (int x = startX; x <= endX; x++)
-        {
-            for (int z = startZ; z <= endZ; z++)
+            // Add one solid pixel around perimeter of the stencil.
+            // Also ledge-extend the perimeter heightmap value for those
+            // non-zero cells, not reducing them at all (they are like
+            // flat flange going outwards that we need in order to later blend).
+            //
+            for (int j = 0; j < h; j++)
             {
-                // why this is inverse who knows it just work
-                heights[z, x] = targetHeight;
+                for (int i = 0; i < w; i++)
+                {
+                    if (source[i, j] == 0)
+                    {
+                        // serves as "hit" or not too
+                        int count = 0;
+
+                        // for average of neighboring heights
+                        float height = 0.0f;
+
+                        for (int neighbor = 0; neighbor < neighborCount; neighbor++)
+                        {
+                            int x = i + neighborXYPairs[neighbor * 2 + 0];
+                            int y = j + neighborXYPairs[neighbor * 2 + 1];
+                            if ((x >= 0) && (x < w) && (y >= 0) && (y < h))
+                            {
+                                // found a neighbor: we will:
+                                //	- areally expand the stencil by this one pixel
+                                //	- sample the neighbor height for the flange extension
+                                if (source[x, y] != 0)
+                                {
+                                    height += heightMap[x, y];
+                                    count++;
+                                }
+                            }
+                        }
+
+                        // extend the height of this cell by the average height
+                        // of the neighbors that contained source stencil true
+                        if (count > 0)
+                        {
+                            expanded[i, j] = 1.0f;
+
+                            extendedHeightmap[i, j] = height / count;
+                        }
+                    }
+                }
+            }
+
+            // Copy the new ledge back to the original heightmap.
+            // WARNING: this is an "output" operation because it is
+            // modifying the supplied input heightmap data, areally
+            // adding around the edge by the pixels encountered.
+            for (int j = 0; j < h; j++)
+            {
+                for (int i = 0; i < w; i++)
+                {
+                    var height = extendedHeightmap[i, j];
+
+                    // only lift... this still allows us to lower terrain,
+                    // since it is lifting from absolute zero to the altitude
+                    // that we actually sensed at this hit neighbor pixels,
+                    // and we need this unattenuated height for later blending.
+                    if (height > 0)
+                    {
+                        heightMap[i, j] = height;
+                    }
+
+                    // zero it too, for next layer (might not be necessary??)
+                    extendedHeightmap[i, j] = 0;
+                }
+            }
+
+            // assign the source to this fresh copy
+            source = expanded;          // shallow copy (reference)
+        }
+
+        // now tally the pile, summarizing each stack of 0/1 solid pixels,
+        // copying it to to the stencil array passed in, which will change
+        // its contents directly, and renormalize it back down to 0.0 to 1.0
+        //
+        // WARNING: this is also an output operation, as it modifies the
+        // blendStencil inbound dataset
+        //
+        for (int j = 0; j < h; j++)
+        {
+            for (int i = 0; i < w; i++)
+            {
+                float total = 0;
+                for (int n = 0; n <= distance; n++)
+                {
+                    total += stencilPile[n][i, j];
+                }
+
+                total /= (distance + 1);
+
+                blendStencil[i, j] = total;
             }
         }
-        terrainData.SetHeights(0, 0, heights);
+
+        // Debug: WritePNG( blendStencil, "blend");
+    }
+
+
+
+    //main function of flattening the terriang base on detecting mesh using raycast
+    public void changeTerrainWhenMesh()
+    {
+        terrain = GameObject.Find("Terrain").GetComponent<ProceduralTerrain>();
+        TerrainData terData = terrain.terrain.terrainData;
+        int Tw = terData.heightmapResolution;
+        int Th = terData.heightmapResolution;
+        var heightMapOriginal = terData.GetHeights(0, 0, Tw, Th);
+        // where we do our work when we generate the new terrain heights
+        var heightMapCreated = new float[heightMapOriginal.GetLength(0), heightMapOriginal.GetLength(1)];
+        // for blending heightMapCreated with the heightMapOriginal to form
+        var heightAlpha = new float[heightMapOriginal.GetLength(0), heightMapOriginal.GetLength(1)];
+
+        terrainTileSize = terrain.width / (float)terrain.resolution;
+        for (int x = 0; x < terrain.resolution; x++)
+        {
+            for (int z = 0; z < terrain.resolution; z++)
+            {
+                Vector3 positionInTerrain = new Vector3(x * terrainTileSize, 200f, z * terrainTileSize);
+                float height = SampleRoadHeight(positionInTerrain);
+                if(height != -1f)
+                {
+                    heightMapCreated[z, x] = (height-0.1f)/ terrain.depth;
+                    heightAlpha[z, x] = 1.0f;
+                }
+            }
+        }
+        // now we might smooth things out a bit
+        if (PerimeterRampDistance > 0)
+        {
+            // Debug: WritePNG( heightMapCreated, "height-0", true);
+            // Debug: WritePNG( heightAlpha, "alpha-0", true);
+
+            GeneratePerimeterHeightRampAndFlange(
+                heightMap: heightMapCreated,
+                blendStencil: heightAlpha,
+                distance: PerimeterRampDistance);
+
+            // Debug: WritePNG( heightMapCreated, "height-1", true);
+            // Debug: WritePNG( heightAlpha, "alpha-1", true);
+        }
+
+        // apply the generated data (blend operation)
+        for (int Tz = 0; Tz < Th; Tz++)
+        {
+            for (int Tx = 0; Tx < Tw; Tx++)
+            {
+                float fraction = heightAlpha[Tz, Tx];
+
+                if (ApplyPerimeterRampCurve)
+                {
+                    fraction = PerimeterRampCurve.Evaluate(fraction);
+                }
+
+                heightMapOriginal[Tz, Tx] = Mathf.Lerp(
+                    heightMapOriginal[Tz, Tx],
+                    heightMapCreated[Tz, Tx],
+                    fraction);
+            }
+        }
+
+        terrain.terrain.terrainData.SetHeights(0, 0, heightMapOriginal);
+    }
+
+    void Start()
+    {
 
     }
-    // soon to be implemented for now looks good
-    public void SetTerrainHeightForArea(int4 area, float targetHeight)
+    // 
+    float SampleRoadHeight(Vector3 position)
     {
-        //area looks like this -> [startx,startz,endx,endz]
-
-        // Get the terrain data
-        TerrainData terrainData = proceduralTerain.terrain.terrainData;
-
-        // Ensure the provided coordinates are within bounds
-        startX = Mathf.Clamp(area[0], 0, terrainData.heightmapResolution - 1);
-        endX = Mathf.Clamp(area[2], 0, terrainData.heightmapResolution - 1);
-        startZ = Mathf.Clamp(area[1], 0, terrainData.heightmapResolution - 1);
-        endZ = Mathf.Clamp(area[3], 0, terrainData.heightmapResolution - 1);
-
-        // Set the height for the specified region
-        float[,] heights = terrainData.GetHeights(0, 0, terrainData.heightmapResolution, terrainData.heightmapResolution);
-
-        int startXEdge = (startX - 1 >= 0) ? startX - 1 : startX;
-        int endXEdge = (endX + 1 <= terrainData.heightmapResolution - 1) ? endX + 1 : endX;
-        int startZEdge = (startZ - 1 >= 0) ? startZ - 1 : startZ;
-        int endZEdge = (endZ + 1 <= terrainData.heightmapResolution - 1) ? endZ + 1 : endZ;
-
-        for(int i = startXEdge; i <= endXEdge; i++)
+        RaycastHit hit;
+        int layerMask = 1 << 3;
+        Ray rayUp = new Ray(position, Vector3.down);
+        if (Physics.Raycast(rayUp, out hit, 200, layerMask))
         {
-            //get heights of neighbour point
-            float terrainHeight = proceduralTerain.GetHeight(i,startZEdge);
-
-            //set height to half of high difference
-            heights[i, startZEdge] = targetHeight - (targetHeight - terrainHeight)/2;
-
-            terrainHeight = proceduralTerain.GetHeight(i, endZEdge);
-
-            heights[i, endZEdge] = targetHeight - (targetHeight - terrainHeight) / 2;
-        }
-
-        for (int i = startZEdge; i <= endZEdge; i++)
-        {
-            float terrainHeight = proceduralTerain.GetHeight(startXEdge, i);
-
-            heights[startXEdge, i] = targetHeight - (targetHeight - terrainHeight) / 2;
-
-            terrainHeight = proceduralTerain.GetHeight(endXEdge, i);
-
-            heights[endXEdge, i] = targetHeight - (targetHeight - terrainHeight) / 2;
-        }
-
-        for (int x = startX; x <= endX; x++)
-        {
-            for (int z = startZ; z <= endZ; z++)
+            // detect if road
+            if (hit.collider != null && hit.collider.GetType() == typeof(MeshCollider))
             {
-                heights[x, z] = targetHeight;
+                return hit.point.y;
             }
         }
+        return -1f;
+    }
+    // Update is called once per frame
+    void Update()
+    {
 
-        terrainData.SetHeights(0, 0, heights);
     }
 }
